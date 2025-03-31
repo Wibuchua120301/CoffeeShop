6ba0671e5fcdf68aaea8dfbdcafb52630aca80b6
Huy Hoang <24130099@st.hcmuaf.edu.vn>
2025-03-31 12:45:43 +0700

    Sửa giao diện quán cafe

--- src/OrderPanel.java
+++ src/OrderPanel.java
@@ -6,40 +6,61 @@ import javax.swing.table.JTableHeader;
 import java.awt.*;
 
 public class OrderPanel extends JFrame {
+    private CoffeeManagement coffeeManagement;
+    private CardLayout cardLayout;
+    private JMenuBar menuBar;
     private JMenu tinhNang;
-    private JMenuItem exit;
+    private JMenuItem action, home, exit;
+    private JPanel billInfoPanel, customerInfoPanel, saveButtonPanel, leftContainer, leftPanel, rightPanel, bottomPanel, orderPanel, main;
     private JButton saveButton;
-    private String[] columnNames;
     private DefaultTableModel tableModel;
     private JTable table;
     private JTableHeader header;
     private JScrollPane scrollPane;
 
     public OrderPanel() {
-        setTitle("Highlands Coffee Management");
-        setSize(900, 500);
+        setTitle("Quản lý quán cà phê");
+        setSize(700, 530);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
+        // cardLayout
+        cardLayout = new CardLayout();
+        main = new JPanel(cardLayout);
+        orderPanel = new JPanel();
+        coffeeManagement = new CoffeeManagement();
+        main.add(orderPanel, "orderPanel");
+        main.add(coffeeManagement, "managementPanel");
+
         // Menu Bar
-        JMenuBar menuBar = new JMenuBar();
-        JMenu tinhNang = new JMenu("Tính năng");
-        JMenuItem exit = new JMenuItem("Exit");
+        menuBar = new JMenuBar();
+        tinhNang = new JMenu("Tính năng");
+        action = new JMenuItem("Quản lý");
+        home = new JMenuItem("Đơn hàng");
+        tinhNang.add(action);
+        tinhNang.add(home);
+        action.addActionListener(e -> {
+            cardLayout.show(main, "managementPanel");
+        });
+        home.addActionListener(e -> {
+            cardLayout.show(main, "orderPanel");
+        });
+        exit = new JMenuItem("Thoát");
         exit.addActionListener(e -> System.exit(0));
         menuBar.add(tinhNang);
         menuBar.add(exit);
         setJMenuBar(menuBar);
 
         // Left Panel - Thông tin hóa đơn và khách hàng
-        JPanel billInfoPanel = new JPanel(new GridLayout(2, 2, 5, 5));
+        billInfoPanel = new JPanel(new GridLayout(2, 2, 5, 5));
         billInfoPanel.setBorder(BorderFactory.createTitledBorder("Thông tin hóa đơn"));
         billInfoPanel.add(new JLabel("ID Hóa đơn:"));
         billInfoPanel.add(new JTextField());
         billInfoPanel.add(new JLabel("Ngày:"));
         billInfoPanel.add(new JTextField());
 
-        JPanel customerInfoPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+        customerInfoPanel = new JPanel(new GridLayout(4, 2, 5, 5));
         customerInfoPanel.setBorder(BorderFactory.createTitledBorder("Thông tin khách hàng"));
         customerInfoPanel.add(new JLabel("Tên:"));
         customerInfoPanel.add(new JTextField());
@@ -50,16 +71,16 @@ public class OrderPanel extends JFrame {
         customerInfoPanel.add(new JLabel("Điểm tích lũy:"));
         customerInfoPanel.add(new JTextField());
 
-        JButton saveButton = new JButton("Lưu");
-        JPanel saveButtonPanel = new JPanel();
+        saveButton = new JButton("Lưu");
+        saveButtonPanel = new JPanel();
         saveButtonPanel.add(saveButton);
 
-        JPanel leftContainer = new JPanel(new BorderLayout());
+        leftContainer = new JPanel(new BorderLayout());
         leftContainer.add(billInfoPanel, BorderLayout.NORTH);
         leftContainer.add(customerInfoPanel, BorderLayout.CENTER);
         leftContainer.add(saveButtonPanel, BorderLayout.SOUTH);
 
-        JPanel leftPanel = new JPanel(new GridBagLayout()); // Dùng GridBagLayout để căn giữa
+        leftPanel = new JPanel(new GridBagLayout()); // Dùng GridBagLayout để căn giữa
         GridBagConstraints gbc = new GridBagConstraints();
         // Chiếm nhiều hàng(gridheight) hoặc nhiều cột (gridWidth)
         // Căn chỉnh theo hướng (anchor)
@@ -73,16 +94,16 @@ public class OrderPanel extends JFrame {
         leftPanel.add(leftContainer, gbc);
 
         // Right Panel - Danh sách sản phẩm
-        JPanel rightPanel = new JPanel(new BorderLayout());
+        rightPanel = new JPanel(new BorderLayout());
         String[] columnNames = {"ID", "Tên", "Kích cỡ", "Số lượng", "Giá", "Thành tiền", "Ghi chú"};
-        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 1 );
-        JTable table = new JTable(tableModel);
-        JTableHeader header = table.getTableHeader();
+        tableModel = new DefaultTableModel(columnNames, 1);
+        table = new JTable(tableModel);
+        header = table.getTableHeader();
         header.setBackground(Color.decode("#B4EBE6"));
-        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane = new JScrollPane(table);
         rightPanel.add(scrollPane, BorderLayout.CENTER);
 
-        JPanel bottomPanel = new JPanel(new GridLayout(1, 5));
+        bottomPanel = new JPanel(new GridLayout(1, 5));
         bottomPanel.add(new JLabel("Tổng: "));
         bottomPanel.add(new JTextField());
         bottomPanel.add(new JTextField());
@@ -91,12 +112,13 @@ public class OrderPanel extends JFrame {
         rightPanel.add(bottomPanel, BorderLayout.SOUTH);
 
         // Add components to frame
-        add(leftPanel, BorderLayout.WEST);
-        add(rightPanel, BorderLayout.CENTER);
+        orderPanel.add(leftPanel, BorderLayout.WEST);
+        orderPanel.add(rightPanel, BorderLayout.CENTER);
+        add(main);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-         new OrderPanel();
+        new OrderPanel();
     }
 }
